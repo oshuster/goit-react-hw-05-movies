2d6123603cc75b56cd50fa5f0d00f13d2b9f4ca7
oshuster <godzilasanya@gmail.com>
2024-01-26 06:58:01 +0200

    added pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { Header } from './Header/Header';
+import { Home } from 'Pages/Home/Home';
+import { Movies } from 'Pages/Movies/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
