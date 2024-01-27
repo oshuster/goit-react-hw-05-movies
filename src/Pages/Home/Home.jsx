165371913f76fbcf6d93eea9cd352b4eb6384719
oshuster <godzilasanya@gmail.com>
2024-01-27 10:28:57 +0200

    migrate

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import { getTopRate } from 'components/api/movieService';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-export const Home = () => {
-  return <div>Home</div>;
+const Home = () => {
+  const topRateData = async () => {
+    const data = await getTopRate();
+    console.log(data);
+  };
+  topRateData();
+
+  return (
+    <ListGroup as="ol" numbered>
+      <ListGroup.Item
+        as="li"
+        className="d-flex justify-content-between align-items-start"
+      >
+        <div className="ms-2 me-auto">
+          <div className="fw-bold">Subheading</div>
+          Cras justo odio
+        </div>
+        <Badge bg="primary" pill>
+          14
+        </Badge>
+      </ListGroup.Item>
+    </ListGroup>
+  );
 };
+
+export default Home;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Header } from './Header/Header';
-import { Home } from 'Pages/Home/Home';
+import Header from './Header/Header';
+import Home from 'Pages/Home/Home';
 import { Movies } from 'Pages/Movies/Movies';
 
 export const App = () => {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-      </Routes>
+      <Home />
     </div>
   );
 };
